8f77624224b52aff675b648528906acd6b8b2118
Emilio <emilio89@gmail.com>
2019-05-07 17:20:28 +0200

    first commit

---
+++ PracticaGit2/src/es/vass/pruebaGit/Ventana.java
@@ -0,0 +1,42 @@
+package es.vass.pruebaGit;
+
+import java.io.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class Ventana
+{
+static Frame awtventana;
+static JFrame swingventana;
+
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      ;
+public void awtventana()
+{
+awtventana= new Frame("Ventana libreria awt") ;
+awtventana.reshape(100,200,400,400);
+awtventana.setLayout(new FlowLayout());
+awtventana.show();	
+}
+
+public void swingventana()
+{
+swingventana= new JFrame("Ventana libreria swing") ;
+swingventana.reshape(550,200,400,400);
+swingventana.setLayout(new FlowLayout());
+swingventana.show();	
+}
+
+public static void main (String [] args)
+ {
+ 	Ventana x=new Ventana();
+ 	x.awtventana();
+ 	x.swingventana();
+ 
+ }
+	
+
+}
